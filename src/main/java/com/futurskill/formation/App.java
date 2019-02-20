72323ab98d4709d7d3864966690f26552fb0f36f
Noémie Dehaene <noemie.dehaene@wanadoo.fr>
2019-02-20 14:11:48 +0100

    mon premier commit

---
+++ src/main/java/com/futurskill/formation/App.java
@@ -0,0 +1,13 @@
+package com.futurskill.formation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
